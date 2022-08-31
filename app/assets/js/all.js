6867fa81e85b20fb4489121e414f8ed15f1b9d7c
amy <amy>
2022-08-31 18:38:59 +0800

    首頁 初版

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,24 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  const swiper = new Swiper(".artist-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    //多欄
+    breakpoints: {
+      1200: {
+        centeredSlides:true,
+        slidesPerView: 3,
+        spaceBetween: 24,
+      }
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination"
+    },
+  });
+
+  // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
+  $('.artwork').imagesLoaded().progress( function() {
+    $('.artwork').masonry(); // 渲染整體畫面
+  });
+
 });
