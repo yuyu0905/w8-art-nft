62a01c13036e821fc424ec70707d0dfaf140410c
amy <amy>
2022-09-02 03:33:45 +0800

    Update 2022-09-01T19:33:42.725Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -30,13 +30,36 @@ $(function () {
     pagination: {
       el: ".swiper-pagination"
     }
+  });
+  var artIntroductionSwiper = new Swiper(".art-introduction-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 24,
+    //多欄
+    breakpoints: {
+      992: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination"
+    }
   }); // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
+  // 首頁
 
   $('.artwork').imagesLoaded().progress(function () {
     $('.artwork').masonry(); // 渲染整體畫面
-  });
+  }); // 探索頁
+
   $('.explore').imagesLoaded().progress(function () {
     $('.explore').masonry(); // 渲染整體畫面
+  }); // 藝術品介紹頁
+
+  $('.art-intro').imagesLoaded().progress(function () {
+    $('.art-intro').masonry(); // 渲染整體畫面
   });
 });
 //# sourceMappingURL=all.js.map
